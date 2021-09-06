14c24d80837d336862b1cf7aa14c9a9105e556d0
bsspencer42 <bsspencer42@gmail.com>
2021-09-06 10:48:50 -0500

    Initial commit

---
+++ MinHeap/src/TestHeap.java
@@ -0,0 +1,8 @@
+public class TestHeap {
+    public static void main(String[] args) {
+        MinHeap<Integer> heap = new MinHeap<>();
+        heap.add(1);
+        heap.add(2);
+        heap.printArray();
+    }
+}
